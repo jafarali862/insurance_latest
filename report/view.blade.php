e3974072a273e1404fd8fcebb64383edeca81b51
Jafarali Ulladan <72119951+jafarali862@users.noreply.github.com>
2025-09-07 22:35:44 +0530

    Update view.blade.php

--- report/view.blade.php
+++ report/view.blade.php
@@ -65,3 +65,43 @@ $('input.select-answer-checkbox-garage').on('change', function () {
                                 <p>Templates</p>
                             </a>
                         </li>
+
+
+
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_template', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')
+                  ->constrained('questions')
+                  ->onDelete('cascade');
+
+            $table->foreignId('template_id')
+                  ->constrained('templates')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_template');
+    }
+};
+
